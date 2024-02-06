d2430668f7de67899ba6bb86e3ff64ff3a703947
Cleberson <pauluci.cleberson@gmail.com>
2024-02-06 20:18:47 -0300

    Refactored the main dart file with wrap custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_adv_basics_challenge/start_screen.dart';
+import 'package:flutter_adv_basics_challenge/quiz.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      title: "Flutter adv basics challenge",
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 61, 246),
-                Color.fromARGB(255, 70, 91, 140),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
-}
+void main() => runApp(
+      const Quiz(),
+    );
