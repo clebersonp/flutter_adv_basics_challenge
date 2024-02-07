587ddb8f52a225d98227e87a9e9bd87939b4d395
Cleberson <pauluci.cleberson@gmail.com>
2024-02-06 21:38:55 -0300

    Passing a function as a constructor parameter to switch the screen

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +27,7 @@ class StartScreen extends StatelessWidget {
           ),
           const SizedBox(height: 50),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(
               foregroundColor: const Color.fromARGB(255, 188, 194, 246),
               textStyle: const TextStyle(
