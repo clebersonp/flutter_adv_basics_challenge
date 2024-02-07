5b3f290c9eef06580d3a210e7b71bbbbbaf6a5cc
Cleberson <pauluci.cleberson@gmail.com>
2024-02-07 09:49:00 -0300

    Used the model quizQuestion to insert text into question and answers manually

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_adv_basics_challenge/answer_button.dart';
+import 'package:flutter_adv_basics_challenge/data/questions.dart';
+import 'package:flutter_adv_basics_challenge/models/quiz_question.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -10,6 +12,8 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  final currentQuestion = questions.first;
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -18,7 +22,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            "Learn Flutter the fun way!",
+            currentQuestion.title,
             style: GoogleFonts.lato(
               fontSize: 26,
               color: const Color.fromARGB(255, 188, 194, 246),
@@ -26,19 +30,19 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           ),
           const SizedBox(height: 30),
           AnswerButton(
-            text: "Answer 1",
+            quizAnswer: currentQuestion.answers[0],
             onTap: () {},
           ),
           AnswerButton(
-            text: "Answer 2",
+            quizAnswer: currentQuestion.answers[1],
             onTap: () {},
           ),
           AnswerButton(
-            text: "Answer 3",
+            quizAnswer: currentQuestion.answers[2],
             onTap: () {},
           ),
           AnswerButton(
-            text: "Answer 4",
+            quizAnswer: currentQuestion.answers[3],
             onTap: () {},
           ),
         ],
