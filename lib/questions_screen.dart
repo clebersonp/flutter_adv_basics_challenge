c46b0f6a63ea8d045769aaface7edc7c336f5da5
Cleberson <pauluci.cleberson@gmail.com>
2024-02-07 13:33:43 -0300

    Receiving the choose function in question_screen widget and pass it to answer_button wiget

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_adv_basics_challenge/answer_button.dart';
 import 'package:flutter_adv_basics_challenge/data/questions.dart';
+import 'package:flutter_adv_basics_challenge/models/answer_question.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({
+    super.key,
+    required this.onSelectedAnswer,
+  });
+
+  final void Function(AnswerQuestion) onSelectedAnswer;
 
   @override
   State<QuestionsScreen> createState() => _QuestionsScreenState();
@@ -13,8 +19,9 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void selectNextQuestion() {
+  void answerQuestion(AnswerQuestion answer) {
     if (currentQuestionIndex < questions.length - 1) {
+      widget.onSelectedAnswer(answer);
       setState(() {
         currentQuestionIndex++;
       });
@@ -37,16 +44,17 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               textAlign: TextAlign.center,
               currentQuestion.title,
               style: GoogleFonts.lato(
-                fontSize: 24,
-                color: const Color.fromARGB(255, 188, 194, 246),
-                fontWeight: FontWeight.bold
-              ),
+                  fontSize: 24,
+                  color: const Color.fromARGB(255, 188, 194, 246),
+                  fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 30),
             ...currentQuestion.shuffleAnswers.map(
               (answer) => AnswerButton(
                 quizAnswer: answer,
-                onTap: selectNextQuestion,
+                onTap: () => answerQuestion(
+                  AnswerQuestion(question: currentQuestion, answer: answer),
+                ),
               ),
             )
           ],
