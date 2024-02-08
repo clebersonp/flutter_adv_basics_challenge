5c52dae2d3b479ca4e315f265f275ce8692dcb55
Cleberson <pauluci.cleberson@gmail.com>
2024-02-07 23:47:33 -0300

    Added style for question summary and result screen widget

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_adv_basics_challenge/models/answer_question.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsSummary extends StatelessWidget {
   final List<AnswerQuestion> summaryData;
@@ -13,17 +14,50 @@ class QuestionsSummary extends StatelessWidget {
         Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text((index + 1).toString()),
-            const SizedBox(width: 10),
+            CircleAvatar(
+              backgroundColor: pickColor(data.userAnswer.correct),
+              foregroundColor:  const Color.fromARGB(255, 20, 6, 105),
+              radius: 18,
+              child: Text(
+                (index + 1).toString(),
+                style: GoogleFonts.lato(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(width: 15),
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(data.question.title, textAlign: TextAlign.justify),
-                  const SizedBox(height: 5),
-                  Text("Correct: ${data.correctAnswer.text}"),
-                  Text("You: ${data.userAnswer.text}"),
-                  const SizedBox(height: 20),
+                  Text(
+                    data.question.title,
+                    textAlign: TextAlign.justify,
+                    style: GoogleFonts.lato(
+                      fontSize: 18,
+                      color: const Color.fromARGB(255, 188, 194, 246),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 2),
+                  Text(
+                    "Correct: ${data.correctAnswer.text}",
+                    style: GoogleFonts.lato(
+                      fontSize: 15,
+                      color: const Color.fromARGB(255, 173, 143, 43),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "You: ${data.userAnswer.text}",
+                    style: GoogleFonts.lato(
+                      fontSize: 15,
+                      color: const Color.fromARGB(255, 43, 162, 173),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
                 ],
               ),
             ),
@@ -45,5 +79,11 @@ class QuestionsSummary extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Color pickColor(bool isCorrect) {
+    return isCorrect
+        ? const Color.fromARGB(150, 55, 148, 243)
+        : const Color.fromARGB(150, 232, 125, 166);
   }
 }
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_adv_basics_challenge/models/answer_question.dart';
 import 'package:flutter_adv_basics_challenge/questions_summary.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({
@@ -28,20 +29,55 @@ class ResultsScreen extends StatelessWidget {
     return SizedBox(
       width: double.infinity,
       child: Container(
-        margin: const EdgeInsets.all(40),
+        margin: const EdgeInsets.all(22),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "You answered ${numCorrectQuestions()} of $totalQuestions questions"
-              " correctly!",
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                text: 'You answered  ',
+                style: GoogleFonts.lato(
+                  fontSize: 24,
+                  color: const Color.fromARGB(255, 188, 194, 246),
+                  fontWeight: FontWeight.bold,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '${numCorrectQuestions()}',
+                    style: const TextStyle(
+                      backgroundColor: Color.fromARGB(255, 43, 162, 173),
+                    ),
+                  ),
+                  const TextSpan(text: ' of '),
+                  TextSpan(
+                    text: '$totalQuestions',
+                    style: const TextStyle(
+                      backgroundColor: Color.fromARGB(255, 173, 143, 43),
+                    ),
+                  ),
+                  const TextSpan(text: ' questions correctly!'),
+                ],
+              ),
             ),
             const SizedBox(height: 30),
             QuestionsSummary(summaryData: chosenAnswers),
             const SizedBox(height: 30),
-            TextButton(
+            TextButton.icon(
               onPressed: reset,
-              child: const Text("Restart Quiz!"),
+              icon: const Icon(
+                Icons.refresh_outlined,
+                color: Color.fromARGB(255, 188, 194, 246),
+                size: 24,
+              ),
+              label: Text(
+                "Restart Quiz!",
+                style: GoogleFonts.lato(
+                  fontSize: 16,
+                  color: const Color.fromARGB(255, 188, 194, 246),
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
             ),
           ],
         ),
